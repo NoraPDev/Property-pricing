ac96ba7f76a5746af875aa3fb2013b1378d66da9
NoraPDev <padarnori@gmail.com>
2023-03-24 19:10:52 +0000

    Remove prediction/predictor temporarily

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from app_pages.multi_page import MultiPage
 # load pages scripts
 from app_pages.Home import home_body
 from app_pages.Analysis import analysis_body
-from app_pages.Prediction import prediction_body
-from app_pages.Predictor import predictor_body
+# from app_pages.Prediction import prediction_body
+# from app_pages.Predictor import predictor_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Price Predictor - Houses")
@@ -19,7 +19,7 @@ app = MultiPage(app_name="Price Predictor - Houses")
 # Add your app pages here using .add_page()
 app.add_page("Home", home_body)
 app.add_page("Analysis", analysis_body)
-app.add_page("House Sale Price Prediction", prediction_body)
-app.add_page("Price Predictor", predictor_body)
+# app.add_page("House Sale Price Prediction", prediction_body)
+# app.add_page("Price Predictor", predictor_body)
 
 app.run()  # Run the app
