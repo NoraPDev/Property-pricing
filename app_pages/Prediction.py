bfafcb203db2378611b4f924270844fda2998019
NoraPDev <padarnori@gmail.com>
2023-03-24 17:42:14 +0000

    Refactor prediction to streamlit 0.85.0

---
+++ app_pages/Prediction.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from tensorflow import keras
+import tensorflow as tf
+import pickle
+
+def prediction_body():
+    st.title('Sales Price Prediction')
+
+    with open('data\important_features.pkl','rb') as f:
+        features=pickle.load(f)
+
+    inherited = pd.read_pickle('data\inherited_houses_corr.pkl')[features]
+    st.header('Inherited houses')
+    st.write(inherited)
+
+    model = keras.models.load_model('data\Model')
+    prediction=model.predict(inherited)
+    inherited_with_prediction=pd.DataFrame(inherited)
+    inherited_with_prediction['predicted SalePrice']=prediction
+    st.header('Inherited houses prediction')
+    st.write(inherited_with_prediction)
