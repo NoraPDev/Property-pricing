951c716908f72f52ac75db51b25d71d962379fe7
NoraPDev <padarnori@gmail.com>
2023-03-26 16:07:38 +0100

    Change packages on app pages

--- app_pages/Predictor.py
+++ app_pages/Predictor.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import plotly.express as px
 from tensorflow import keras
+import tensorflow as tf
 import pickle
 
 def predictor_body():
