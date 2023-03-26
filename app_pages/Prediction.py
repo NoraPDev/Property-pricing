f352e6adee02ca1a118b38cbbcd7d59007b1d6d7
NoraPDev <padarnori@gmail.com>
2023-03-26 16:51:30 +0100

    Add packages to prediction.py

--- app_pages/Prediction.py
+++ app_pages/Prediction.py
@@ -1,22 +1,24 @@
+import os
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
 from tensorflow import keras
-import tensorflow as tf
 import pickle
 
 def prediction_body():
+    important_features_file = os.path.join(os.getcwd(), "data", "important_features.pkl")
+    inherited_houses_corr_file = os.path.join(os.getcwd(), "data", "inherited_houses_corr.pkl")
+    model_folder = os.path.join(os.getcwd(), "data", "Model")
     st.title('Sales Price Prediction')
 
-    with open('data\important_features.pkl','rb') as f:
+    with open(important_features_file,'rb') as f:
         features=pickle.load(f)
 
-    inherited = pd.read_pickle('data\inherited_houses_corr.pkl')[features]
+    inherited = pd.read_pickle(inherited_houses_corr_file)[features]
     st.header('Inherited houses')
     st.write(inherited)
-
-    model = keras.models.load_model('data\Model')
+    
+    model = keras.models.load_model(model_folder)
+   
     prediction=model.predict(inherited)
     inherited_with_prediction=pd.DataFrame(inherited)
     inherited_with_prediction['predicted SalePrice']=prediction
